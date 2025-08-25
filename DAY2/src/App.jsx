f7187b8b887fb74c00b3f3a1efcc89cdcafdf1df
sidhantrajput1 <sidhant808310@gmail.com>
2025-08-25 11:13:02 +0530

    Accordion component done

--- DAY2/src/App.jsx
+++ DAY2/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CharacterCounter from './component/CharacterCounter';
 import TabsComponet from './component/TabsComponet';
+import Accordion from './component/Accordion';
 
 const App = () => {
   return (
     <div>
       {/* <CharacterCounter /> */}
-      <TabsComponet />
+      {/* <TabsComponet /> */}
+      <Accordion />
     </div>
   );
 }
